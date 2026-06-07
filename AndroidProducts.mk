75681850cf0ec9778dfc6833785589fffeaa9c15
wuyichen0531-coder <wuyichen0531@gmail.com>
2026-06-07 13:55:41 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,3 +8,10 @@ PRODUCT_MAKEFILES := \
 COMMON_LUNCH_CHOICES := \
     fox_PHQ110-eng \
     fox_PHQ110-userdebug
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/fox_PHQ110.mk
+
+COMMON_LUNCH_CHOICES := \
+    fox_PHQ110-eng \
+    fox_PHQ110-userdebug
